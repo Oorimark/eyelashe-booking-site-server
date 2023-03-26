5b72290995920bbd43f0047e3fc4d32caa59d5c9
Oorimark <oorimark@gmail.com>
2023-03-26 12:14:05 +0100

    change app password

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'markpublicm@gmail.com'
-app.config['MAIL_PASSWORD'] = 'xodvtzyiphvkhrmu'
+app.config['MAIL_PASSWORD'] = 'gbtmsvxjxbhpoekf'
 app.config['MAIL_DEFAULT_SENDER'] = ('Lashedout_bc','markpublicm@gmail.com')
 app.config['MAIL_ASCII_ATTACHMENTS'] = False
 
